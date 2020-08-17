dcde5e787162e8b5ac091ecd4b6942e2aa7a8304
adityavamshid <68259533+adityavamshid@users.noreply.github.com>
2020-08-17 08:20:34 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,37 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","D:\\Workspace\\SeleniumJavaProject\\chromedriver.exe");
+		ChromeDriver chrome = new ChromeDriver();
+		chrome.get("https://www.google.com/");
+		System.out.println("Browser opened successfully");
+		chrome.findElement(By.name("q")).sendKeys("selenium");
+		
+		chrome.findElement(By.className("gNO89b")).click();
+		System.out.println(chrome.getCurrentUrl());
+		System.out.println(chrome.getTitle());
+		chrome.navigate().refresh();
+		chrome.get("https://www.yahoo.com/");
+		chrome.navigate().back();
+		chrome.navigate().forward();
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+
+	}
+
+}
